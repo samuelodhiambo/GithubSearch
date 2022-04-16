20a99e5f4103fd6cce62bdce585812298904f395
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-16 23:30:52 +0300

    api: It will add api key to environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  githubSeachAPIKey: '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'
 };
 
 /*
