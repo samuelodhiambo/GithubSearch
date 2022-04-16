75bfe3d18eb1b070c0c6a082b79519c34cffda52
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-17 00:33:31 +0300

    ft: It will add main component

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  title = 'Github Search'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  doSearch (term: string) {
+    console.log(term)
+  }
+
+}
