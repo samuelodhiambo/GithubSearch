09b32d770e7f5b7e330f3e3c01722ecf69889a20
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-19 09:28:24 +0300

    patch: It will add search method on search Service

--- src/app/services/search-service.service.ts
+++ src/app/services/search-service.service.ts
@@ -1,10 +1,61 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    Authorization: 'access_token:' + environment.githubSeachAPIKey
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchServiceService {
+  results: any[] = []
+  ghUser?: any;
 
   constructor(private http: HttpClient) { }
+
+  search(term: string) {
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http.get(`https://api.github.com/users/${term}/repos`, httpOptions).subscribe({
+        next: (res: any) => {
+          console.log(res)
+          this.results = res;
+          resolve();
+        },
+        error: (err: any) => {
+          reject(err);
+        },
+        complete: () => {
+          console.log('complete')
+        }
+      });
+      
+    });
+    return promise
+  }
+
+  searchUser(term: string) {
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http.get(`https://api.github.com/users/${term}`, httpOptions).subscribe({
+        next: (res: any) => {
+          console.log(res)
+          this.ghUser = res;
+          resolve();
+        },
+        error: (err: any) => {
+          reject(err);
+        },
+        complete: () => {
+          console.log('complete')
+        }
+      });
+      
+    });
+    return promise
+  }
 }
