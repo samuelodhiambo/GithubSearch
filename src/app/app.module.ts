9b4ab289f57a38cbeaa4785708e7b295e5921e63
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-19 09:18:04 +0300

    patch: It will import Search Service and add it to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { SearchServiceService } from './services/search-service.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { MainComponent } from './components/main/main.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [SearchServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
