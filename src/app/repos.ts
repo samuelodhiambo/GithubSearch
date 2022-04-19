04969ec8b74517ade80874562af25f58253c766c
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-19 09:21:03 +0300

    patch: It will add repos class

---
+++ src/app/repos.ts
@@ -0,0 +1,7 @@
+import { Url } from "url";
+
+export class Repos {
+    constructor(id: number, name: string, owner: any[], html_url: Url, created_at: Date, topics: any[], stargazers_count: number, forks_count: number) {
+        
+    }
+}
