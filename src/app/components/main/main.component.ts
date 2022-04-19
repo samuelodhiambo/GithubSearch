d5cbca81f8168e2081833fd65a1d45b2230e7585
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-19 09:25:21 +0300

    patch: It will add avator url from search service

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchServiceService } from 'src/app/services/search-service.service';
 
 @Component({
   selector: 'app-main',
@@ -8,13 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
   title = 'Github Search'
 
-  constructor() { }
+  constructor(public searchService: SearchServiceService) { 
+    this.title = 'Github Search'
+  }
 
   ngOnInit(): void {
+    this.searchService.searchUser('samuelodhiambo')
   }
 
   doSearch (term: string) {
-    console.log(term)
+    this.searchService.search(term)
   }
 
 }
